062519aeb70f60702af8b2b18fa49890a8856c2f
Johnny Fekete <hello@johnnyfekete.com>
2020-11-11 14:07:12 +0100

    collapsing page effect with example site

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import { ExampleComponent } from 'collapsing-page'
-import 'collapsing-page/dist/index.css'
+import CollapsingPage from 'collapsing-page';
+import 'collapsing-page/dist/index.css';
+
+import logo from './logo.png';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [destroy, setDestroy] = useState(false);
 
-export default App
+  return (
+    <>
+      <div className="page">
+        <div className="header">
+          <nav>
+            <ul>
+              <li>
+                <img src={logo} alt="Collapsing Page Demo" className="logo" />
+              </li>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#users">Users</a>
+              </li>
+              <li>
+                <a href="#settings">Settings</a>
+              </li>
+              <li>
+                <button type="button" onClick={() => setDestroy(true)}>
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="content">
+          <div className="articles">
+            <article>
+              <div>
+                <h2>Article title</h2>
+                <p>
+                  Posted on{' '}
+                  <time dateTime="2020-11-11T16:31:24+02:00">
+                    November 11th 2020
+                  </time>{' '}
+                  by <a href="#author">Johnny Fekete</a> -{' '}
+                  <a href="#comments">6 comments</a>
+                </p>
+              </div>
+              <p>
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </p>
+            </article>
+
+            <article>
+              <div>
+                <h2>Article title</h2>
+                <p>
+                  Posted on{' '}
+                  <time dateTime="2020-09-04T16:31:24+02:00">
+                    September 4th 2020
+                  </time>{' '}
+                  by <a href="#author">Johnny Fekete</a> -{' '}
+                  <a href="#comments">6 comments</a>
+                </p>
+              </div>
+              <p>
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </p>
+            </article>
+          </div>
+
+          <aside>
+            <h2>About section</h2>
+            <p>
+              Donec eu libero sit amet quam egestas semper. Aenean ultricies mi
+              vitae est. Mauris placerat eleifend leo.
+            </p>
+          </aside>
+        </div>
+
+        <footer>
+          <p>2020</p>
+        </footer>
+      </div>
+      <CollapsingPage destroy={destroy}>
+        <div className="after-collapse">
+          <h1>This will stay when everything else's gone</h1>
+        </div>
+      </CollapsingPage>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,140 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const CollapsingPage = ({ destroy, duration, onFinish, children }) => {
+  // Reference for the content, that will stay after the collapsing effect
+  const contentRef = useRef(false);
+
+  // If the prop `destroy` becomes true, execute the collapse effect
+  useEffect(() => {
+    if (!destroy) {
+      return;
+    }
+
+    // Get all elements that are immediate children of body, and all divs
+    const elements = document.querySelectorAll('body > *, body div');
+
+    // Get the total height of the document
+    const { body } = document;
+    const html = document.documentElement;
+
+    const height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+
+    const originalOverflow = document.body.style.overflow;
+    const originalPointerEvents = document.body.style.pointerEvents;
+    document.body.style.overflow = 'hidden';
+    document.body.style.pointerEvents = 'none';
+
+    let maxDuration = 0;
+
+    // Loop through all elements that will be animated
+    [...elements].forEach((element) => {
+      // Ignore the content and its children, that will stay on screen after the logout
+      if (
+        element === contentRef.current ||
+        isDescendant(contentRef.current, element)
+      ) {
+        element.style.pointerEvents = 'all';
+        return;
+      }
+
+      // Get original values for each element, so they can be moved back after the animation is done
+      const originalValues = {
+        pointerEvents: element.style.pointerEvents,
+        transition: element.style.transition,
+        transitionDelay: element.style.transitionDelay,
+        transform: element.style.transform
+      };
+
+      element.setAttribute(
+        'data-original-values',
+        JSON.stringify(originalValues)
+      );
+
+      element.style.pointerEvents = 'none';
+
+      // Calculate unique animation values
+      const delay = Math.random() * (duration * 0.375); // wait between 0 and 3 seconds
+      const rotate = Math.random() * 30 - 15; // rotate with max 10 degrees
+      const moveX = Math.random() * 160 - 80; // move with 50px to either direction
+      const speed = Math.random() * (duration * 0.375) + duration * 0.25; // speed between 2 and 5 seconds
+
+      if (maxDuration < delay + speed) {
+        maxDuration = delay + speed;
+      }
+
+      element.style.transition = `transform ${speed}ms ease-out`;
+      element.style.transitionDelay = `${delay}ms`;
+      element.style.transform = `translateY(${
+        height * 1.5
+      }px) translateX(${moveX}px) rotate(${rotate}deg)`;
+    });
+
+    setTimeout(() => onFinish(), maxDuration);
+
+    // Destructor - move back the elements to their original position
+    return () => {
+      document.body.style.overflow = originalOverflow;
+      document.body.style.pointerEvents = originalPointerEvents;
+
+      [...elements].forEach((element) => {
+        if (element.getAttribute('data-original-values')) {
+          const originalValues = JSON.parse(
+            element.getAttribute('data-original-values')
+          );
+
+          element.style.pointerEvents = originalValues.pointerEvents;
+          element.style.transition = originalValues.transition;
+          element.style.transitionDelay = originalValues.transitionDelay;
+          element.style.transform = originalValues.transform;
+        }
+      });
+    };
+  }, [destroy]);
+
+  if (!destroy) {
+    return null;
+  }
+
+  return createPortal(<div ref={contentRef}>{children}</div>, document.body);
+};
+
+CollapsingPage.propTypes = {
+  destroy: PropTypes.bool,
+  duration: PropTypes.number,
+  onFinish: PropTypes.func,
+  children: PropTypes.node
+};
+
+CollapsingPage.defaultProps = {
+  destroy: false,
+  duration: 8000,
+  onFinish: () => {},
+  children: null
+};
+
+export default CollapsingPage;
+
+/**
+ * Check, if an element is a descendant of another in the DOM
+ *
+ * @param DOMElement parent
+ * @param DOMElement child
+ */
+const isDescendant = (parent, child) => {
+  let node = child.parentNode;
+  while (node !== null) {
+    if (node === parent) {
+      return true;
+    }
+    node = node.parentNode;
+  }
+  return false;
+};
